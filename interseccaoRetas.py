b7d0543d0e0ba74702968e58f56fb101991cae91
wagnerfns <wagnerfelidre@gmail.com>
2018-12-28 20:46:33 -0300

    referente aula 00

---
+++ interseccaoRetas.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,100,10)
+y = 4*x - 35
+
+plt.title('Questao 03: Interseccao retas')
+plt.plot(pontos,x)
+plt.plot(pontos,y)
+plt.show()
